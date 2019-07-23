419d2e5bd92addd65eb8374a402b101c5436c575
nicole josquin <nicole.gerbauld@gmail.com>
2019-07-23 14:57:40 +0800

    updated task manager files

---
+++ test/controllers/task_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class TaskControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get task_new_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get task_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get task_edit_url
+    assert_response :success
+  end
+
+end
